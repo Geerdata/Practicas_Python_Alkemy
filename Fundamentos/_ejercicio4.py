05556785f541ca13c710b566e9098527429456db
Geerdata <rodriguezgermancarlos@gmail.com>
2024-03-25 17:45:13 -0300

    Fundamentos - Ejercicio 4

--- Fundamentos/_ejercicio4.py
+++ Fundamentos/_ejercicio4.py
@@ -1,6 +1,17 @@
-dni = input('Ingrese su DNI').strip()
-edad = input('Ingrese su Edad').strip()
+dni = input('Ingrese su DNI: ').strip()
+edad = input('Ingrese su Edad: ').strip()
 
 
-if not (dni or edad) : 
+
+if not (dni and edad): 
     print('Usted no ha ingreso su DNI ,ni edad por lo tanto puede generar la cuenta')
+else:
+    edad = int(edad)
+    if edad <= 12:
+        print('No se puede crear una cuenta por su edad')
+    elif 13 <= edad <= 17:
+        print('Puedes tener una cuenta con control parental')
+    else:
+        print('Puede tener una cuenta con acceso total')
+
+    
