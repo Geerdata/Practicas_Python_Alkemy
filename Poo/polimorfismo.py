6d507527c191067bfa28ff219960ddcf2ddd962f
Geerdata <rodriguezgermancarlos@gmail.com>
2024-03-28 20:33:20 -0300

    Polimorficos

---
+++ Poo/polimorfismo.py
@@ -0,0 +1,43 @@
+class Rectangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+        self.__area = None
+
+    def calcular_area(self):
+        self.__area = self.base * self.altura
+        return self.__area
+    
+    def get_area(self):
+        return self.__area
+    
+
+class Circulo:
+    def __init__(self, radio):
+        self.radio = radio
+        self.__area = None
+
+    def calcular_area(self):
+        self.__area = 3.14 * self.radio ** 2 
+        return self.__area
+    
+    def get_area(self):
+        return self.__area
+
+
+def obtener_area(figura):
+    if hasattr(figura, 'calcular_area'):
+        return figura.calcular_area()
+
+    return 0
+
+rectangulo = Rectangulo(4,6)
+circulo = Circulo(10)
+
+print(f'Area del rectangulo: {obtener_area(rectangulo)}')
+print(f'Area del circulo: {obtener_area(circulo)}')
+
+print(f'Area del rectangulo:  {rectangulo._Rectangulo__area} ')
+
+
+
